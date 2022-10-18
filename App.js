fb26ed69bf51bc592a91a1861f0fdd71478f46f6
Erik Behrends <behrends@users.noreply.github.com>
2022-10-18 15:31:15 +0200

    Storage in eigener Klasse unter lib

--- App.js
+++ App.js
@@ -2,27 +2,9 @@ import { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import Constants from 'expo-constants';
 import { StyleSheet, View } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import Todos from './components/Todos';
 import NewTodo from './components/NewTodo';
-
-const storeData = async (value) => {
-  try {
-    const jsonValue = JSON.stringify(value);
-    await AsyncStorage.setItem('todos', jsonValue);
-  } catch (e) {
-    // TODO: handle saving error
-  }
-};
-
-const getData = async () => {
-  try {
-    const jsonValue = await AsyncStorage.getItem('todos');
-    return jsonValue != null ? JSON.parse(jsonValue) : [];
-  } catch (e) {
-    // TODO: handle error reading value
-  }
-};
+import Storage from './lib/storage';
 
 export default function App() {
   const [todos, setTodos] = useState([]);
@@ -37,11 +19,11 @@ export default function App() {
     const newTodo = { id: todos.length, text: todo };
     const newTodos = [...todos, newTodo];
     setTodos(newTodos);
-    storeData(newTodos);
+    Storage.storeData(newTodos);
   }
 
   async function loadTodos() {
-    const myTodos = await getData();
+    const myTodos = await Storage.getData();
     setTodos(myTodos);
   }
 
