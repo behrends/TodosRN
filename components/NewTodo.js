c9e7a9475510828d35fae01e02b591390be8f38c
Erik Behrends <behrends@users.noreply.github.com>
2022-10-18 13:43:32 +0200

    NewTodo horizontal ausgerichtet mit FlexBox

--- components/NewTodo.js
+++ components/NewTodo.js
@@ -1,15 +1,20 @@
-import { Button, StyleSheet, TextInput } from 'react-native';
+import { Button, StyleSheet, TextInput, View } from 'react-native';
 
 export default function NewTodo() {
   return (
-    <>
+    <View style={styles.container}>
       <TextInput style={styles.input} placeholder="Todo eingeben" />
       <Button title="Speichern" />
-    </>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
   input: {
     width: '80%',
     borderWidth: 1,
