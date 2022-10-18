515422ad3d28b68766f62336b33bc77637cc7148
Erik Behrends <behrends@users.noreply.github.com>
2022-10-18 11:14:39 +0200

    Beispiel Fortsetzung

--- components/Todo.js
+++ components/Todo.js
@@ -1,12 +1,6 @@
 import { StyleSheet, Text } from 'react-native';
 
-export default function Todo(props) {
-  // Destrukturierung
-  const { children, id } = props;
-  // …entspricht folgenden Zeilen
-  // const children = props.children;
-  // const id = props.id;
-
+export default function Todo({ children, id }) {
   const todoText = `${id} — ${children}`;
 
   return <Text style={styles.text}>{todoText}</Text>;
