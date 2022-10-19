95e12d600aecaa71e40bc370f6a93269f28a1271
Erik Behrends <behrends@users.noreply.github.com>
2022-10-19 15:37:36 +0200

    Screens in eigenen Dateien

--- App.js
+++ App.js
@@ -1,76 +1,17 @@
-import { useEffect, useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import Constants from 'expo-constants';
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Todos from './components/Todos';
-import NewTodo from './components/NewTodo';
-import Storage from './lib/storage';
-
-function SettingsScreen() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Text>Settings!</Text>
-    </View>
-  );
-}
+import Home from './screens/Home';
+import Settings from './screens/Settings';
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
-  const [todos, setTodos] = useState([]);
-
-  // Lade Daten aus AsyncStorage
-  // beim erstmaligen Start der App
-  useEffect(() => {
-    loadTodos();
-  }, []); // <-- einmalige Ausführen
-
-  function addTodo(todo) {
-    const newTodo = { id: todos.length, text: todo };
-    const newTodos = [...todos, newTodo];
-    setTodos(newTodos);
-    Storage.storeData(newTodos);
-  }
-
-  async function loadTodos() {
-    const myTodos = await Storage.getData();
-    setTodos(myTodos);
-  }
-
-  function HomeScreen() {
-    return (
-      <View style={styles.container}>
-        <NewTodo onAddTodo={addTodo} />
-        <Todos todos={todos} />
-        <StatusBar style="auto" />
-      </View>
-    );
-  }
-
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Settings" component={Settings} />
       </Tab.Navigator>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
-  },
-});
---
+++ screens/Home.js
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import Constants from 'expo-constants';
+import Todos from '../components/Todos';
+import NewTodo from '../components/NewTodo';
+import Storage from '../lib/storage';
+
+export default function Home() {
+  const [todos, setTodos] = useState([]);
+
+  // Lade Daten aus AsyncStorage
+  // beim erstmaligen Start der App
+  useEffect(() => {
+    loadTodos();
+  }, []); // <-- einmalige Ausführen
+
+  function addTodo(todo) {
+    const newTodo = { id: todos.length, text: todo };
+    const newTodos = [...todos, newTodo];
+    setTodos(newTodos);
+    Storage.storeData(newTodos);
+  }
+
+  async function loadTodos() {
+    const myTodos = await Storage.getData();
+    setTodos(myTodos);
+  }
+
+  return (
+    <View style={styles.container}>
+      <NewTodo onAddTodo={addTodo} />
+      <Todos todos={todos} />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+  },
+});
